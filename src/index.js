26b6dba1082e959f1ca17c8a20affe4e5f551789
jeffigy <evangelistajeffy25@gmail.com>
2023-05-20 20:09:43 +0800

    POST request

--- src/index.js
+++ src/index.js
@@ -1,39 +1,33 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+//  use method allows you to add middleware to your application's 
+//  request processing pipeline
+app.use(express.json());
+//  parses incoming requests with URL-encoded payloads
+app.use(express.urlencoded())
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
-app.get('/', (req, res) => {
 
-    res.send(
-        [
-            {
-                id: 1,
-                name: 'John',
-            },
-            {
-                id: 2,
-                name: 'Doe',
-            }
-        ]
-    );
-});
-
+const groceries = [
+    {
+        id: 1,
+        item: 'Milk',
+    },
+    {
+        id: 2,
+        item: 'Eggs',
+    }
+]
 
 
 app.get('/groceries', (req, res) => {
-    res.send(
-        [
-            {
-                id: 1,
-                item: 'Milk',
-            },
-            {
-                id: 2,
-                item: 'Eggs',
-            }
-        ]
-    )
-}) 
+    res.send(groceries)
+})
+
+app.post('/groceries', (req, res) => {
+    console.log(req.body);
+    res.send(201)
+})
