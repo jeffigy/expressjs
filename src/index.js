c0e77d6ce66a49413f570c7a8759c9270b20a6f9
jeffigy <evangelistajeffy25@gmail.com>
2023-05-22 09:54:17 +0800

    routes pahabol

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+//registered groceries route, if not register url will return 404
+const groceriesRouter = require('./routes/groceries');
+const marketsRouter = require('./routes/market');
 const express = require('express');
 const app = express();
 const port = 5000;
@@ -11,48 +14,13 @@ app.use((req, res, next) => {
     next();
 })
 
+// invoke the groceries router
+// a very common pattern is to prefix all the routes defined in a router with /api
+app.use('/api/groceries', groceriesRouter);
+app.use('/api/markets', marketsRouter);
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
 
-const groceries = [
-    {
-        id: 1,
-        item: 'Milk',
-    },
-    {
-        id: 2,
-        item: 'Eggs',
-    }
-]
 
-
-app.get('/groceries', (res, req, next) => {
-    // this is begin called before the next function
-    console.log('before handling the request');
-    // await console.log('before handling the request');
-    // if the next function is not called, the request will be left hanging
-    // and the function below will not be processed
-    next();
-
-}, (req, res) => {
-    //next function is used to call the next middleware
-    res.send(groceries)
-})
-app.get('/groceries/:id', (req, res) => {
-    //destructure the id from the request parameters
-    const { id } = req.params;
-    //find the grocery item with the id
-    const grocery = groceries.find((grocery) => grocery.id === parseInt(id));
-    //if the grocery item is not found, return a 404 error
-    if (!grocery) {
-        res.status(404).send('Grocery item not found');
-    }
-    //if the grocery item is found, return the grocery item
-    res.send(grocery);
-})
-
-app.post('/groceries', (req, res) => {
-    console.log(req.body);
-    res.send(201)
-})
